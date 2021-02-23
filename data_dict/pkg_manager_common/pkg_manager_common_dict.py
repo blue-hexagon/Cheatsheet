1ab54bb914404fad8783ed331b998caa8d14c47c
smokingcuke <w0j8uhv5csio@opayq.net>
2021-02-23 10:00:23 +0100

    Create one example for each category of package manager. Set a markup wrapper on all commands and descriptions.

--- data_dict/pkg_manager_common/pkg_manager_common_dict.py
+++ data_dict/pkg_manager_common/pkg_manager_common_dict.py
@@ -49,7 +49,7 @@ cheatsheet = [
 	{
 		'heading': helper.set_folder('Yum'),
 		'subtitle': 'Yum Package Manager',
-		'description': '',
+		'description': 'The Yellowdog Updater, Modified (YUM) package manager is a package manager for Linux operating systems.',
 		'columns': 'col-lg-6 col-md-12',
 		'uuid': helper.get_uuid(),
 		'static_red': '06070c0bd3294269bcd329c87d9861de',
@@ -61,7 +61,7 @@ cheatsheet = [
 			'data': [
 				{
 					'static_red': Markup(helper.set_entry_folder('ae5629debca54de98fe4d93355a90ef4')[0]),
-					'flag': helper.set_entry_command_string('yum list updates'),
+					'flag':  Markup(helper.set_entry_command_string('yum list updates')),
 					'description': Markup('Display list of updated software and security fixes'),
 					'video': Markup(''),
 					'example': helper.example_path(),
@@ -72,9 +72,12 @@ cheatsheet = [
 	},
 	{
 		'heading': helper.set_folder('Pacman'),
-		'subtitle': 'Yum Package Manager',
+		'subtitle': '',
 		'description': '.',
 		'columns': 'col-lg-6 col-md-12',
+		'warning': {
+			'Flags are case-sensitive with pacman: -S is not the same as -s'
+		},
 		'uuid': helper.get_uuid(),
 		'static_red': '1e43dc7bcc12416e952afbe5a72c7b80',
 		'content': {
@@ -85,8 +88,8 @@ cheatsheet = [
 			'data': [
 				{
 					'static_red': Markup(helper.set_entry_folder('637023eae1e44f09bf458096b50c51f7')[0]),
-					'flag': helper.set_entry_command_string(''),
-					'description': Markup(''),
+					'flag':  Markup(helper.set_entry_command_string('pacman -Syu pkg')),
+					'description': Markup('Install pkg and update the package list'),
 					'video': Markup(''),
 					'example': helper.example_path(),
 					'uuid': helper.get_uuid(),
@@ -96,8 +99,8 @@ cheatsheet = [
 	},
 	{
 		'heading': helper.set_folder('Apt'),
-		'subtitle': 'Yum Package Manager',
-		'description': '.',
+		'subtitle': 'Advanced Package Tool',
+		'description': '',
 		'columns': 'col-lg-6 col-md-12',
 		'uuid': helper.get_uuid(),
 		'static_red': '083bb8dc06e8419798d61f3adfa78031',
@@ -109,8 +112,8 @@ cheatsheet = [
 			'data': [
 				{
 					'static_red': Markup(helper.set_entry_folder('39f779066e6d46d385b310b419a37933')[0]),
-					'flag': helper.set_entry_command_string(''),
-					'description': Markup(''),
+					'flag':  Markup(helper.set_entry_command_string('apt-get install pkg')),
+					'description': Markup('Install pkg'),
 					'video': Markup(''),
 					'example': helper.example_path(),
 					'uuid': helper.get_uuid(),
@@ -120,8 +123,8 @@ cheatsheet = [
 	},
 	{
 		'heading': helper.set_folder('Aptitude'),
-		'subtitle': 'Yum Package Manager',
-		'description': '.',
+		'subtitle': '',
+		'description': '',
 		'columns': 'col-lg-6 col-md-12',
 		'uuid': helper.get_uuid(),
 		'static_red': 'f6231a566d504aebabbe724a6cb6950a',
@@ -133,8 +136,8 @@ cheatsheet = [
 			'data': [
 				{
 					'static_red': Markup(helper.set_entry_folder('9f9dea01192c44a4a41bc9eba0d9536f')[0]),
-					'flag': helper.set_entry_command_string(''),
-					'description': Markup(''),
+					'flag':  Markup(helper.set_entry_command_string('aptitude install pkg')),
+					'description': Markup('Install pkg'),
 					'video': Markup(''),
 					'example': helper.example_path(),
 					'uuid': helper.get_uuid(),
@@ -144,8 +147,8 @@ cheatsheet = [
 	},
 	{
 		'heading': helper.set_folder('RPM'),
-		'subtitle': 'Yum Package Manager',
-		'description': '.',
+		'subtitle': 'Redhat Package Manager',
+		'description': 'Not only used for Red Hat Linux',
 		'columns': 'col-lg-6 col-md-12',
 		'uuid': helper.get_uuid(),
 		'static_red': '01bb1d23aa3447b9ab7f1b4d920795e4',
@@ -157,8 +160,8 @@ cheatsheet = [
 			'data': [
 				{
 					'static_red': Markup(helper.set_entry_folder('8341aabf7d0141698e5afd1c25c438a9')[0]),
-					'flag': helper.set_entry_command_string(''),
-					'description': Markup(''),
+					'flag': Markup(helper.set_entry_command_string('rpm -i pkg</kbd><br><kbd>rpm --install pkg')),
+					'description': Markup('Installs pkg'),
 					'video': Markup(''),
 					'example': helper.example_path(),
 					'uuid': helper.get_uuid(),
@@ -168,7 +171,7 @@ cheatsheet = [
 	},
 	{
 		'heading': helper.set_folder('DNF'),
-		'subtitle': 'Yum Package Manager',
+		'subtitle': 'Dandified YUM',
 		'description': '.',
 		'columns': 'col-lg-6 col-md-12',
 		'uuid': helper.get_uuid(),
@@ -181,8 +184,8 @@ cheatsheet = [
 			'data': [
 				{
 					'static_red': Markup(helper.set_entry_folder('165c0ef83add4f4fba376ac8a110967a')[0]),
-					'flag': helper.set_entry_command_string(''),
-					'description': Markup(''),
+					'flag':  Markup(helper.set_entry_command_string('dnf install pkg')),
+					'description': Markup('Install pkg'),
 					'video': Markup(''),
 					'example': helper.example_path(),
 					'uuid': helper.get_uuid(),
@@ -192,8 +195,8 @@ cheatsheet = [
 	},
 	{
 		'heading': helper.set_folder('Zypper'),
-		'subtitle': 'Yum Package Manager',
-		'description': '.',
+		'subtitle': '',
+		'description': '',
 		'columns': 'col-lg-6 col-md-12',
 		'uuid': helper.get_uuid(),
 		'static_red': '48784b81a3a84dfe975e583f2d3fcf41',
@@ -205,8 +208,8 @@ cheatsheet = [
 			'data': [
 				{
 					'static_red': Markup(helper.set_entry_folder('a3ed754d95e448cfb615248653024924')[0]),
-					'flag': helper.set_entry_command_string(''),
-					'description': Markup(''),
+					'flag':  Markup(helper.set_entry_command_string('zypper in pkg</kbd><br><kbd>zypper install pkg')),
+					'description': Markup('Install pkg'),
 					'video': Markup(''),
 					'example': helper.example_path(),
 					'uuid': helper.get_uuid(),
@@ -216,8 +219,8 @@ cheatsheet = [
 	},
 	{
 		'heading': helper.set_folder('NPM'),
-		'subtitle': 'Yum Package Manager',
-		'description': '.',
+		'subtitle': 'Node Package Manager',
+		'description': '',
 		'columns': 'col-lg-6 col-md-12',
 		'uuid': helper.get_uuid(),
 		'static_red': '5b7d0b1087494dbe99d3ef773e0c92bb',
@@ -229,8 +232,8 @@ cheatsheet = [
 			'data': [
 				{
 					'static_red': Markup(helper.set_entry_folder('f47f6631760e4e3a95539001134abfdf')[0]),
-					'flag': helper.set_entry_command_string(''),
-					'description': Markup(''),
+					'flag':  Markup(helper.set_entry_command_string('npm install pkg')),
+					'description': Markup('Install pkg'),
 					'video': Markup(''),
 					'example': helper.example_path(),
 					'uuid': helper.get_uuid(),
@@ -240,8 +243,8 @@ cheatsheet = [
 	},
 	{
 		'heading': helper.set_folder('pip'),
-		'subtitle': 'Yum Package Manager',
-		'description': '.',
+		'subtitle': '',
+		'description': '',
 		'columns': 'col-lg-6 col-md-12',
 		'uuid': helper.get_uuid(),
 		'static_red': 'fd3518f7ceaa464caf6ef5f71ce57b99',
@@ -253,32 +256,8 @@ cheatsheet = [
 			'data': [
 				{
 					'static_red': Markup(helper.set_entry_folder('0e730f82c8c44e83aea595508800b0ee')[0]),
-					'flag': helper.set_entry_command_string(''),
-					'description': Markup(''),
-					'video': Markup(''),
-					'example': helper.example_path(),
-					'uuid': helper.get_uuid(),
-				},
-			]
-		}
-	},
-	{
-		'heading': helper.set_folder('PyPi'),
-		'subtitle': 'Yum Package Manager',
-		'description': '.',
-		'columns': 'col-lg-6 col-md-12',
-		'uuid': helper.get_uuid(),
-		'static_red': '9dc246246e3f42e3b47752f57877985d',
-		'content': {
-			'descriptor': [
-				'Command',
-				'Description'
-			],
-			'data': [
-				{
-					'static_red': Markup(helper.set_entry_folder('e8f0fd201c1b4e6eb5b26e9c7fa37109')[0]),
-					'flag': helper.set_entry_command_string(''),
-					'description': Markup(''),
+					'flag':  Markup(helper.set_entry_command_string('pip install pkg')),
+					'description': Markup('Install pkg'),
 					'video': Markup(''),
 					'example': helper.example_path(),
 					'uuid': helper.get_uuid(),
